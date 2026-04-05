757d10f284949adf2ae7400e6b8ceaaaf7da4d2d
Archit Tyagi <archittyagi462@gmail.com>
2026-04-05 21:37:40 +0530

    Fix: auto-create DB directory before SQLite init (Render deployment fix)

--- server/src/db/sqlite.js
+++ server/src/db/sqlite.js
@@ -1,6 +1,7 @@
 import Database from 'better-sqlite3';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,7 +9,14 @@ const __dirname = dirname(__filename);
 let db;
 
 export function initDB() {
-  db = new Database(join(__dirname, '../../data/finbridge.db'));
+  const dbPath = join(__dirname, '../../data/finbridge.db');
+  const dbDir = dirname(dbPath);
+  
+  if (!fs.existsSync(dbDir)) {
+    fs.mkdirSync(dbDir, { recursive: true });
+  }
+
+  db = new Database(dbPath);
   db.pragma('journal_mode = WAL');
 
   db.exec(`
